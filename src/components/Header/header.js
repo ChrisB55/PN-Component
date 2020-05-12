bde7b3b1a9f0eba6c82b7861aa4881304d8c4d35
Chris <christopherwilliambrown@gmail.com>
2020-05-12 15:21:59 -0400

    inital commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,72 @@
+import React from "react"
+import "./styles.css"
+
+export default () => (
+  <header id="main-header">
+    <div class="header clearfix">
+      <div class="top-area">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3 col-sm-6"></div>
+            <div class="col-md-6 col-sm-6"></div>
+            <div class="col-md-3"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="z-logm-container">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 col-sm-12">
+              <div class="logo">
+                <a class="logo-zone" href="/">
+                  {/*  <img src="images/LogoPublica.png" alt="logo" /> */}
+                </a>
+              </div>
+
+              <div class="right-area">
+                <svg
+                  class="bi bi-list"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M2.5 11.5A.5.5 0 013 11h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 7h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5zm0-4A.5.5 0 013 3h10a.5.5 0 010 1H3a.5.5 0 01-.5-.5z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+
+                <nav class="header-nav">
+                  <ul id="navigation">
+                    <li class="active">
+                      <a href="/">Home</a>
+                    </li>
+                    <li>
+                      <a href="about">About</a>
+                    </li>
+                    <li>
+                      <a href="services">Services</a>
+                    </li>
+                    <li>
+                      <a href="clients">Clients</a>
+                    </li>
+                    <li>
+                      <a href="testimonals">Testimonials</a>
+                    </li>
+                    <li>
+                      <a href="#contact">Contact Us</a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../components/Header/header"
+import Footer from "../components/footer"
+
+export default () => (
+  <div>
+    <Header />
+    <h1>Hero Component</h1>
+    <Footer />
+  </div>
+)
