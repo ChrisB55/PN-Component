33e8755947be4c2c8b5a722beab8d87ad5e2de27
Chris <christopherwilliambrown@gmail.com>
2020-05-14 09:33:20 -0400

    add hero image component

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./styles.css"
+
+export default function Hero() {
+  return (
+    <div class="hero-image">
+      <div class="hero-text">
+        <h1>Modern React</h1>
+        <p> Solving old and new problems with modern state management. </p>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Header from "../components/Header/header"
 import Footer from "../components/Footer/footer"
+import Hero from "../components/Hero/hero"
 
 export default () => (
   <div>
     <Header />
-    <h1>Hero Component</h1>
+    <Hero />
     <Footer />
   </div>
 )
